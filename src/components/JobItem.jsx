82d306dfa3407a96cfab44e6fb42efd1d1cb9685
Ulises Vaneki Daniel <vanekiulises@gmail.com>
2026-02-26 01:58:57 -0300

    Added applicationId

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -21,6 +21,7 @@ export function JobItem({ job, candidate, onSubmitApplication }) {
         uuid: candidate.uuid,
         jobId: job.id,
         candidateId: candidate.candidateId,
+        applicationId: candidate.applicationId,
         repoUrl: repoUrl,
       });
       setStatus('success');
